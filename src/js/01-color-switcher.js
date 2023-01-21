f81f2150ee6b08e0238ab192cae793edc5e71d1d
vitalij99 <vitalisklymko@gmail.com>
2023-01-21 22:25:19 +0200

    switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+const start = document.querySelector("[data-start]")
+const stop = document.querySelector("[data-stop]")
+
+start.addEventListener("click", getRemoveInterval)
+stop.addEventListener("click", getRemoveInterval)
+
+let timerId = null;
+
+function getRemoveInterval({ target }) {  
+    
+    if (target.dataset.start !== undefined) {
+        target.disabled = true
+        stop.disabled = false
+        getBodyColor()
+        timerId = setInterval(getBodyColor, 1000)
+                
+    } else {
+        target.disabled = true
+        start.disabled = false        
+        clearInterval(timerId)        
+    }  
+}
+
+function getBodyColor() {    
+    document.body.style.backgroundColor = getRandomHexColor()
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
